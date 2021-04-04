27eeb0083f753a2ec6146a17a1669a4c4719e0cf
Artvin2001 <azaborovskaya01@gmail.com>
2021-04-04 21:58:22 +0300

    Code style

--- src/com/company/lesson4/Main.java
+++ src/com/company/lesson4/Main.java
@@ -17,23 +17,29 @@ public class Main {
     public static void main(String[] args) {
         initMap();
         printMap();
+
         while (true) {
             humanTurn();
             printMap();
+
             if (checkWin(DOT_X)) {
                 System.out.println("Победил человек");
                 break;
             }
+
             if (isMapFull()) {
                 System.out.println("Ничья");
                 break;
             }
+
             aiTurn();
             printMap();
+
             if (checkWin(DOT_O)) {
                 System.out.println("Победил Искуственный Интеллект");
                 break;
             }
+
             if (isMapFull()) {
                 System.out.println("Ничья");
                 break;
@@ -72,6 +78,7 @@ public class Main {
 
         //проверка главной диагонали
         flag = true;
+
         for (int i = 0; i < DOTS_TO_WIN; i++) {
             if (map[i][i] != symb)
                 flag = false;
@@ -102,33 +109,40 @@ public class Main {
                 if (map[i][j] == DOT_EMPTY) return false;
             }
         }
+
         return true;
     }
     public static void aiTurn() {
         int x, y;
+
         do {
             x = rand.nextInt(SIZE);
             y = rand.nextInt(SIZE);
         } while (!isCellValid(x, y));
+
         System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
         map[y][x] = DOT_O;
     }
     public static void humanTurn() {
         int x, y;
+
         do {
             System.out.println("Введите координаты в формате X Y");
             x = sc.nextInt() - 1;
             y = sc.nextInt() - 1;
         } while (!isCellValid(x, y)); // while(isCellValid(x, y) == false)
+
         map[y][x] = DOT_X;
     }
     public static boolean isCellValid(int x, int y) {
         if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
+
         if (map[y][x] == DOT_EMPTY) return true;
         return false;
     }
     public static void initMap() {
         map = new char[SIZE][SIZE];
+
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 map[i][j] = DOT_EMPTY;
@@ -139,7 +153,9 @@ public class Main {
         for (int i = 0; i <= SIZE; i++) {
             System.out.print(i + " ");
         }
+
         System.out.println();
+
         for (int i = 0; i < SIZE; i++) {
             System.out.print((i + 1) + " ");
             for (int j = 0; j < SIZE; j++) {
@@ -147,6 +163,7 @@ public class Main {
             }
             System.out.println();
         }
+
         System.out.println();
     }
 }
